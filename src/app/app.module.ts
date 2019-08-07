baf45c40f82ec3cb48a5efe05c0637972261a0d9
sjoseph1848 <sjoseph1848@gmail.com>
2019-08-06 22:15:04 -0500

    updated the charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,15 @@ import { FormsComponent } from './core/components/forms/forms.component';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatIconModule,MatSidenavModule,MatListModule} from '@angular/material';
+import { SidenavListComponent } from './core/components/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FormsComponent
+    FormsComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,10 @@ import {MatInputModule} from '@angular/material/input';
     MatToolbarModule,
     MatSelectModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
     
   ],
   providers: [],
