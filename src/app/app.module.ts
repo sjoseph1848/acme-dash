100b75a51d6cf54b570c87ba2fa49df1f0eb0eb0
Sam <sjoseph1848@gmail.com>
2019-08-11 18:43:23 -0500

    upated project component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
 import { FormsComponent } from './core/components/forms/forms.component';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule,MatSidenavModule,MatListModule} from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule
+} from '@angular/material';
 import { WelcomeComponent } from './page/welcome/welcome.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './core/components/navigation/header/header.component';
 import { SidenavListComponent } from './core/components/navigation/sidenav-list/sidenav-list.component';
+import { MatCardModule } from '@angular/material/card';
+import { ProjectComponent } from './dashboard/project/project.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,7 +36,8 @@ import { SidenavListComponent } from './core/components/navigation/sidenav-list/
     LoginComponent,
     DashboardComponent,
     HeaderComponent,
-    SidenavListComponent
+    SidenavListComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
@@ -41,10 +50,12 @@ import { SidenavListComponent } from './core/components/navigation/sidenav-list/
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    FlexLayoutModule
-    
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/dashboard/project/project.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
+})
+export class ProjectComponent implements OnInit {
+  project = {};
+
+  constructor() {}
+
+  ngOnInit() {}
+}
